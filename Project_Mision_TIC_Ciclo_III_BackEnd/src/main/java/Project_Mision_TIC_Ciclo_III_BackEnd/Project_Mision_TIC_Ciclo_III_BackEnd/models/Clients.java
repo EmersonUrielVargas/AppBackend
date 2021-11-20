ce2065b10a3f8bbbcfdf91a27c25fa9ade6776da
EmersonUrielVargas <71224875+EmersonUrielVargas@users.noreply.github.com>
2021-11-20 08:48:47 -0500

    ultimo commit

--- Project_Mision_TIC_Ciclo_III_BackEnd/src/main/java/Project_Mision_TIC_Ciclo_III_BackEnd/Project_Mision_TIC_Ciclo_III_BackEnd/models/Clients.java
+++ Project_Mision_TIC_Ciclo_III_BackEnd/src/main/java/Project_Mision_TIC_Ciclo_III_BackEnd/Project_Mision_TIC_Ciclo_III_BackEnd/models/Clients.java
@@ -3,8 +3,10 @@ package Project_Mision_TIC_Ciclo_III_BackEnd.Project_Mision_TIC_Ciclo_III_BackEn
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 public class Clients {
 	@Id
--- Project_Mision_TIC_Ciclo_III_BackEnd/src/main/java/Project_Mision_TIC_Ciclo_III_BackEnd/Project_Mision_TIC_Ciclo_III_BackEnd/models/Sales.java
+++ Project_Mision_TIC_Ciclo_III_BackEnd/src/main/java/Project_Mision_TIC_Ciclo_III_BackEnd/Project_Mision_TIC_Ciclo_III_BackEnd/models/Sales.java
@@ -8,6 +8,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({"hibernateLazyInitializer"})
 @Entity
 public class Sales {
 	@Id
